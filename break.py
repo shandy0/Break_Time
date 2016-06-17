aabc823b1bef44ecc1cc89f16c05e7920fc6452c
Shivam Singh <iec2014016@iiita.ac.in>
2016-06-16 23:36:19 -0700

    main program file

---
+++ break.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+total_breaks = 3
+break_count = 0
+print("This program started on "+time.ctime())
+while (break_count<total_breaks):
+     time.sleep(10)
+     webbrowser.open("https://www.youtube.com/watch?v=d_VfA1yveAE")
+     break_count=break_count+1
